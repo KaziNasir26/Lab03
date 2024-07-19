287d8236ca910302d4d9a7d6e4bf90cf89bcc4ae
Kazi Fahad Bin Nasir <kazifahadbin.nasir@ucalgary.ca>
2024-07-18 19:24:51 -0600

    working on university class

--- src/ca/ucalgary/ensf380/Person.java
+++ src/ca/ucalgary/ensf380/Person.java
@@ -16,5 +16,48 @@ public abstract class Person {
         this.personalNumber = ++lastPersonalNumber;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public int getPersonalNumber() {
+        return personalNumber;
+    }
+
+    public static String nextPersonalNumber(short type) {
+        return String.valueOf(lastPersonalNumber + 1);
+    }
+
+    public boolean hasValidAddress() {
+        return address != null && address.validate();
+    }
+}
 
