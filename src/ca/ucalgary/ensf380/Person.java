850a70fce61896c59e6de12caea489b068b6448b
Kazi Fahad Bin Nasir <kazifahadbin.nasir@ucalgary.ca>
2024-07-18 19:55:54 -0600

    works on abstract

--- src/ca/ucalgary/ensf380/Person.java
+++ src/ca/ucalgary/ensf380/Person.java
@@ -1,12 +1,6 @@
 package ca.ucalgary.ensf380;
 
-public abstract class Person {
-    private String name;
-    private String phoneNumber;
-    private String emailAddress;
-    private Address address;
-    private static int lastPersonalNumber = 0;
-    private int personalNumber;
+
 
     public Person(String name, String phoneNumber, String emailAddress, Address address) {
         this.name = name;
